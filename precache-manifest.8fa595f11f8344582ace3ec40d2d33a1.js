39a4fd020a7ec6daa2cddb9c637c99921820408c
Sanu-github <sanudutt45@gmail.com>
2020-03-07 00:30:19 +0530

    Updates

---
+++ precache-manifest.8fa595f11f8344582ace3ec40d2d33a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf62b5e274a0bed50cecb68bee867ce",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "79f0759a51bb39031134",
+    "url": "/react-pages/static/js/2.f1da216a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-pages/static/js/2.f1da216a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaecbc871ba536285afe",
+    "url": "/react-pages/static/js/main.df9dee87.chunk.js"
+  },
+  {
+    "revision": "2664a39fa5c0d92b12f0",
+    "url": "/react-pages/static/js/runtime-main.90787385.js"
+  }
+]);
